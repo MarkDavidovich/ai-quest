acc5fbd23298f837424c2ebec40a919acb654b48
Mark <mark.davidovich@gmail.com>
2026-03-24 22:14:40 +0200

    add better styling to combat ui

--- frontend/src/components/CombatUI/CombatUI.jsx
+++ frontend/src/components/CombatUI/CombatUI.jsx
@@ -1,10 +1,17 @@
 import style from "./CombatUI.module.css";
 
-const CombatUI = ({ hp, maxHp, name }) => {
+const CombatUI = ({ type, hp, maxHp, name }) => {
+  const hpPercent = Math.max(0, Math.min(100, (hp / maxHp) * 100));
+
   return (
     <div className={style.container}>
-      <div>{name}</div>
-      <div>{hp + "/" + maxHp}</div>
+      <div className={style.name}>{name}</div>
+      <div className={style.hpBarContainer}>
+        <div className={style.hpBarFill} style={{ width: `${hpPercent}%` }}></div>
+      </div>
+      <div className={style.hpText}>
+        {hp} / {maxHp}
+      </div>
     </div>
   );
 };
