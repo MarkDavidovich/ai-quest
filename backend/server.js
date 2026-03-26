8dee62d1e18c1c9f34263e209d1c7f1bb76dea91
Mark <mark.davidovich@gmail.com>
2026-03-26 22:39:11 +0200

    attempt to fix render bug #2

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use("/auth", authRouter);
 app.use("/api/npc-dialogue", npcDialogueRoutes);
 
 // Catch-all route to serve the React app
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
