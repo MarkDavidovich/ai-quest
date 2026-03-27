5692c75c6496d31b43ee22eaea92b6a494d7e770
Ronbi1 <bitonr7@gmail.com>
2026-03-27 19:57:22 +0300

    chore: Add package-lock.json and .gitignore to lock dependencies and ignore common files.

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -3,44 +3,21 @@ const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    // static associate(models) {
-    //   User.hasOne(models.PlayerProfile, {
-    //     foreignKey: 'user_id',
-    //     as: 'profile'
-    //   });
-
-    //   User.hasMany(models.Inventory, {
-    //     foreignKey: 'user_id',
-    //     as: 'inventory' // השם שישמש אותנו כשנשלוף את התיק של השחקן
-    //   });
-
-    //   User.hasOne(models.GameSassion, {
-    //     foreignKey: 'user_id',
-    //     as: 'session'
-    //   });
-    // }
     static associate(models) {
-      // 1. נדפיס למסך אילו מודלים Sequelize באמת טען
-      console.log("🔍 Available models in Sequelize:", Object.keys(models));
+      User.hasOne(models.PlayerProfile, {
+        foreignKey: 'user_id',
+        as: 'profile'
+      });
 
-      // 2. נעטוף ב-If כדי למנוע קריסה בזמן הדיבוג
-      if (models.PlayerProfile) {
-        User.hasOne(models.PlayerProfile, { foreignKey: 'user_id', as: 'profile' });
-      } else {
-        console.log("❌ models.PlayerProfile is undefined!");
-      }
+      User.hasMany(models.Inventory, {
+        foreignKey: 'user_id',
+        as: 'inventory'
+      });
 
-      if (models.Inventory) {
-        User.hasMany(models.Inventory, { foreignKey: 'user_id', as: 'inventory' });
-      } else {
-        console.log("❌ models.Inventory is undefined!");
-      }
-
-      if (models.GameSession) {
-        User.hasOne(models.GameSession, { foreignKey: 'user_id', as: 'session' });
-      } else {
-        console.log("❌ models.GameSession is undefined!");
-      }
+      User.hasOne(models.GameSession, {
+        foreignKey: 'user_id',
+        as: 'session'
+      });
     }
   }
 
