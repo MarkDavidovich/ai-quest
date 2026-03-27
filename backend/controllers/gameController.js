b2c9b434d9921aef6e625dc9ed6a5702361d82a2
Ronbi1 <bitonr7@gmail.com>
2026-03-27 17:29:37 +0300

    update name of file + add load game session function

--- backend/controllers/gameController.js
+++ backend/controllers/gameController.js
@@ -1,5 +1,7 @@
+const { where } = require('sequelize');
 const { default: Player } = require('../../frontend/src/components/Player/Player');
 const { PlayerProfile, GameSession, Inventory, sequelize } = require('../db/models');
+const inventory = require('../db/models/inventory');
 
 exports.saveGameState = async (req, res) => {
     const userId = req.user.id;
@@ -41,3 +43,38 @@ exports.saveGameState = async (req, res) => {
         res.status(500).json({ error: 'Failed to save game state' });
     }
 }
+
+exports.loadGameState = async (req, res){
+    const userId = req.user.id;
+
+    try {
+        const [profile, session, inventoryItems] = await Promise.all([
+            PlayerProfile.findOne({ where: { user_id: userId } }),
+            GameSession.findOne({ where: { user_id: userId } }),
+            Inventory.findAll({ where: { user_id: userId } })
+        ]);
+
+        if (!profile || !session) {
+            return res.status(404).json({ message: 'No saved game found for this user.' });
+        }
+
+        const inventory = inventoryItems.map(item => ({
+            name: item.item_name,
+            quantity: item.quantity
+        }));
+        res.status(200).json({
+            profile,
+            session,
+            inventory
+        });
+
+    } catch (error) {
+        console.error('Load game error:', error);
+        res.status(500).json({ error: 'Failed to load game state.' });
+    }
+
+}
+
+
+
+
