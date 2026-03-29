640d58a4597cb502be8ee9d047d609bd1e5cbcb2
Mark <mark.davidovich@gmail.com>
2026-03-29 16:51:11 +0300

    add a more robust as a debugging tool

--- frontend/src/components/GameUI/GameUI.jsx
+++ frontend/src/components/GameUI/GameUI.jsx
@@ -1,12 +1,12 @@
 import styles from "./GameUI.module.css";
 
-const GameUI = ({ playerGridPos, playerDisplayPos, message, gridWidth, gridHeight, facingDir }) => {
+const GameUI = ({ playerGridPos, playerDisplayPos, message, gridWidth, gridHeight, facingDir, currentMapId }) => {
   return (
     <div className={styles.statusDisplay}>
       <p className={styles.statusText}>
         <strong>Grid Position:</strong> ({playerGridPos.x}, {playerGridPos.y}) | <strong style={{ marginLeft: "12px" }}>Display Position:</strong> (
-        {playerDisplayPos.x.toFixed(2)}, {playerDisplayPos.y.toFixed(2)}) | <strong style={{ marginLeft: "12px" }}>World:</strong> {gridWidth}×{gridHeight}{" "}
-        tiles
+        {playerDisplayPos.x.toFixed(2)}, {playerDisplayPos.y.toFixed(2)}) | <strong style={{ marginLeft: "12px" }}>MapId:</strong> {currentMapId} |{" "}
+        <strong style={{ marginLeft: "12px" }}>World:</strong> {gridWidth}×{gridHeight} tiles
       </p>
       <p>{`Facing direction: x:${facingDir.x}, y:${facingDir.y}`}</p>
       <p className={styles.message}>{message}</p>
